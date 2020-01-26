6b679b52defc5ecd11bd9b3906089ff2a5171a2d
ich777 <ich777@noreply.example.org>
2020-01-26 21:16:01 +0100

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -4,6 +4,15 @@ export DISPLAY=:99
 echo "---Setting umask to ${UMASK}---"
 umask ${UMASK}
 
+echo "---Preparing Server---"
+echo "---Checking for old logfiles---"
+find $DATA_DIR -name "XvfbLog.*" -exec rm -f {} \;
+find $DATA_DIR -name "x11vncLog.*" -exec rm -f {} \;
+echo "---Checking for old lock files---"
+find /tmp -name ".X99*" -exec rm -f {} \;
+find /var/run/dbus -name "pid" -exec rm -f {} \;
+chmod -R 777 ${DATA_DIR}
+
 echo "---Starting Xvfb server---"
 screen -S Xvfb -L -Logfile ${DATA_DIR}/XvfbLog.0 -d -m /opt/scripts/start-Xvfb.sh
 sleep 2
@@ -16,5 +25,5 @@ echo "---Starting noVNC server---"
 websockify -D --web=/usr/share/novnc/ --cert=/etc/ssl/novnc.pem 8080 localhost:5900
 sleep 2
 
-echo "---Sleep zZz---"
-sleep infinity
+echo "---Starting Desktop---"
+mate-desktop
