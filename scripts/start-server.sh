5d9be960992b80c1b77b3d9b66a3fdba1b8c5047
ich777 <ich777@noreply.example.org>
2020-01-26 19:53:06 +0100

    „scripts/start-server.sh“ hinzufügen

---
+++ scripts/start-server.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+export LANG=en_US.UTF-8
+export DISPLAY=:99
+echo "---Setting umask to ${UMASK}---"
+umask ${UMASK}
+
+echo "---Starting Xvfb server---"
+screen -S Xvfb -L -Logfile ${DATA_DIR}/XvfbLog.0 -d -m /opt/scripts/start-Xvfb.sh
+sleep 5
+
+echo "---Starting x11vnc server---"
+screen -S x11vnc -L -Logfile ${DATA_DIR}/x11vncLog.0 -d -m /opt/scripts/start-x11.sh
+sleep 5
+
+echo "---Starting noVNC server---"
+websockify -D --web=/usr/share/novnc/ --cert=/etc/ssl/novnc.pem 8080 localhost:5900
+sleep 5
+
+sleep infinity
