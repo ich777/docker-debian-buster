4c6c9e878aaf42411859d9fbbff0e3a051ef8a67
ich777 <ich777@noreply.example.org>
2020-01-26 19:53:34 +0100

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -6,14 +6,15 @@ umask ${UMASK}
 
 echo "---Starting Xvfb server---"
 screen -S Xvfb -L -Logfile ${DATA_DIR}/XvfbLog.0 -d -m /opt/scripts/start-Xvfb.sh
-sleep 5
+sleep 2
 
 echo "---Starting x11vnc server---"
 screen -S x11vnc -L -Logfile ${DATA_DIR}/x11vncLog.0 -d -m /opt/scripts/start-x11.sh
-sleep 5
+sleep 2
 
 echo "---Starting noVNC server---"
 websockify -D --web=/usr/share/novnc/ --cert=/etc/ssl/novnc.pem 8080 localhost:5900
-sleep 5
+sleep 2
 
+echo "---Sleep zZz---"
 sleep infinity
