cf9ceeef1b79e7458019bf18d7e62acbbe2bbe91
ich777 <ich777@noreply.example.org>
2020-05-01 19:32:35 +0200

    „scripts/start.sh“ ändern

--- scripts/start.sh
+++ scripts/start.sh
@@ -16,9 +16,65 @@ if [ -f /opt/scripts/user.sh ]; then
 else
 	echo "---No optional script found, continuing---"
 fi
+if [ -z "${PCI_ADDR}" ]; then
+echo "---Trying to get Nvidia device address---"
+export PCI_ADDR="$(nvidia-smi | grep 0000 | cut -d '|' -f3 | cut -d ':' -f2,3 | cut -d ' ' -f1)"
+	if [ -z "${PCI_ADDR}" ]; then
+		echo "---Something went wrong, can't get device address, putting server into sleep mode---"
+		sleep infinity
+	else
+		echo "---Successfully got device ID: ${PCI_ADDR}---"
+	fi
+PCI_ADDR=PCI:"${PCI_ADDR/./:}"
+fi
+
+if [ -z "${NV_DRV_V}" ]; then
+echo "---Trying to get Nvidia driver version---"
+export NV_DRV_V="$(nvidia-smi | grep NVIDIA-SMI | cut -d ' ' -f3)"
+	if [ -z "${NV_DRV_V}" ]; then
+		echo "---Something went wrong, can't get driver version, putting server into sleep mode---"
+		sleep infinity
+	else
+		echo "---Successfully got driver version: ${NV_DRV_V}---"
+	fi
+fi
+
+echo "---Checking Xwrapper.config---"
+if grep -rq 'allowed_users=anybody' /etc/X11/Xwrapper.config; then
+	echo "---Xwrapper.config properly configured---"
+else
+	echo "---Configuring Xwrapper.config---"
+	sed -i '/allowed_users=/c\allowed_users=anybody' /etc/X11/Xwrapper.config
+fi
+
+INSTALL_V="$(find ${DATA_DIR} -name NVIDIA_*\.run | cut -d '_' -f 2 | cut -d '.' -f 1,2)"
+
+if [ ! -z "$INSTALL_V" ]; then
+	if [ "$INSTALL_V" != "${NV_DRV_V}" ]; then
+		echo "---Version missmatch, deleting local Nvidia Driver v$INSTALL_V---"
+		rm ${DATA_DIR}/NVIDIA_$INSTALL_V.run
+	fi
+fi
+
+if [ ! -f /usr/bin/nvidia-settings ]; then
+	if [ -f ${DATA_DIR}/NVIDIA_${NV_DRV_V}.run ]; then
+		echo "---Found NVIDIA Driver v${NV_DRV_V} localy, installing...---"
+		${DATA_DIR}/NVIDIA_${NV_DRV_V}.run ${NVIDIA_BUILD_OPTS} > /dev/null 2>&1
+	else
+		echo "---Downloading and installing Nvidia Driver v${NV_DRV_V}---"
+		wget -q --show-progress --progress=bar:force:noscroll -O /tmp/NVIDIA.run http://download.nvidia.com/XFree86/Linux-x86_64/${NV_DRV_V}/NVIDIA-Linux-x86_64-${NV_DRV_V}.run && \
+		chmod +x /tmp/NVIDIA.run && \
+		/tmp/NVIDIA.run ${NVIDIA_BUILD_OPTS} > /dev/null 2>&1 && \
+		mv /tmp/NVIDIA.run ${DATA_DIR}/NVIDIA_${NV_DRV_V}.run
+	fi
+fi
+
+sed -i "/BusID/c\\\tBusID\t\"${PCI_ADDR}\"" /etc/X11/xorg.conf
+sed -i "/Option\\t\"ConnectedMonitor\"/c\\\tOption\t\"ConnectedMonitor\" \"DFP-${DFP_NR}\"" /etc/X11/xorg.conf
+sed -i "/Option\\t\"CustomEDID\"/c\\\tOption\t\"CustomEDID\" \"DFP${DFP_NR}:${DATA_DIR}/edid.txt\"" /etc/X11/xorg.conf
 
 if [ ! -d /tmp/xdg ]; then
-	mkdir /tmp/xdg
+	mkdir -p /tmp/xdg
 fi
 
 echo "---Configuring Locales to: ${USER_LOCALES}---"
@@ -41,6 +97,9 @@ fi
 echo "---Starting...---"
 rm -R ${DATA_DIR}/.dbus/session-bus/* 2> /dev/null
 chown -R ${UID}:${GID} /opt/scripts
+if [ ! -f ${DATA_DIR}/edid.txt ]; then
+	cp /tmp/edid.txt ${DATA_DIR}/edid.txt
+fi
 chown -R ${UID}:${GID} /tmp/xdg
 chmod -R 0700 /tmp/xdg
 dbus-uuidgen > /var/lib/dbus/machine-id
