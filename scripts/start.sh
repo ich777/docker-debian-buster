09f9e648b7f88a407c4789e1a3fd90026c457c6a
ich777 <ich777@noreply.example.org>
2020-05-06 21:58:21 +0200

    „scripts/start.sh“ ändern

--- scripts/start.sh
+++ scripts/start.sh
@@ -21,6 +21,8 @@ if [ ! -d /tmp/xdg ]; then
 	mkdir /tmp/xdg
 fi
 
+sed -i "/  <user>/c\  <user>${USER}</user>" /usr/share/dbus-1/system.conf
+
 echo "---Configuring Locales to: ${USER_LOCALES}---"
 LOCALE_GEN=$(head -n 1 /etc/locale.gen)
 export LOCALE_USR=$(echo ${USER_LOCALES} | cut -d ' ' -f 1)
